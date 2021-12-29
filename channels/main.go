3c1dc326e5407a274ff2388d64eaccfe6abf63c6
Maneesh Chauhan <chauhan.maneesh@gmail.com>
2021-12-29 23:21:04 +0200

    added a simple example of usage of channels to signal back the completion of the Get call

--- channels/main.go
+++ channels/main.go
@@ -15,18 +15,29 @@ func main() {
 		"https://www.linkedin.com",
 	}
 
+	// Create a channel through which go routines can communicate with each other
+	// make a channel through which go routines can pass string values
+	ch := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, ch)
+	}
+
+	// wait for messages on the channel for all the corresponding checkLink method calls
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-ch)
 	}
 }
 
 // check if the weblink is responding to the get request
-func checkLink(link string) {
+func checkLink(link string, ch chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be Down")
+		ch <- "Might be Down"
 		return
 	}
 
 	fmt.Println(link, "is Up")
+	ch <- "Link is Up"
 }
